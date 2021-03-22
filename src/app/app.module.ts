34abf4504a7b73569d28ec48fb1e3e2b7885247a
Jerry <GerardoGonzalezCode@gmail.com>
2021-03-22 15:41:21 -0600

    adus-interface v1.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserTableComponent } from './user-table/user-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserTableComponent
   ],
   imports: [
     BrowserModule,
